01c44a61754a0035b9c5fae01b7911eeadb90893
viktoriiakamorina <107555386+viktoriiakamorina@users.noreply.github.com>
2024-10-28 10:44:06 +0100

    Update wrapperComponent.js

--- wrapperComponent.js
+++ wrapperComponent.js
@@ -20,9 +20,7 @@
   // Dynamically loading the ES module
   loadModule('https://sapocplkwebcomponents.blob.core.windows.net/static/plk-app-div-web-components.esm.js')
       .then(() => {
-          // You can now use the imported module functions or components
-          // If the module exports components or methods you'd like to use globally,
-          // set them up here, for example:
+          // If the module exports components or methods that we need to use globally, set them up here, for example:
           global.MyCustomElement = customElements.get('my-custom-element');
       })
       .catch(error => {
